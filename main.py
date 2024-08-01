4a86729ed0197f53f3957df143e1934a9dd360c1
stillyourpassenger <mariaderbysheva@gmail.com>
2024-08-01 14:23:38 +0200

    add print report

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    print_report(book_path)
+    
+def print_report(book_path):
+    text = read_book_text(book_path)
+    print(f"--- Begin report of {book_path} ---")
+    
+    number_of_words = count_words(text)
+    print(f"{number_of_words} words found in the document\n")
+    
+    characters_counted = count_characters(text)
+    sorted_dict = dict(sorted(characters_counted.items(), key=lambda x:x[1], reverse=True))
+    for k in sorted_dict.keys():
+        print(f"The {k} character was found {sorted_dict[k]} times")
+    
+    print("--- End report ---")
+
 def read_book_text(path):
     with open(path) as f:
         return f.read()
@@ -9,12 +27,9 @@ def count_characters(text):
     text = text.lower()
     text_dict = dict()
     for i in set(text):
-        text_dict[i] = text.count(i)
+        if i.isalpha():
+            text_dict[i] = text.count(i)
     return text_dict
 
-def main():
-    book_path = "books/frankenstein.txt"
-    text = read_book_text(book_path)
-    print(count_characters(text))
 
 main()
